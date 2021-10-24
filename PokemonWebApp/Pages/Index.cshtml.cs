3cc2e78cd8644bd4be4fe0152cf7cfb87bebc838
Alberto Araujo <albertoig8@hotmail.com>
2021-10-23 23:20:45 -0300

    Criando metodos para acessar a API

--- PokemonWebApp/Pages/Index.cshtml.cs
+++ PokemonWebApp/Pages/Index.cshtml.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PokemonWebApp.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace PokemonWebApp.Pages
 {
@@ -15,6 +19,28 @@ namespace PokemonWebApp.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+        }
+
+        string baseUrl = "https://localhost:44375/";
+
+        public async Task OnGetAsync()
+        {
+            List<Pokemon> pokemons = new List<Pokemon>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/Pokemons");
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(result);
+                }
+            }
         }
 
         public void OnGet()
