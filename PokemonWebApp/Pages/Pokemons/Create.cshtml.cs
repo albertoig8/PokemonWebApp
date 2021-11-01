cbadabdabc58ea1274b8fffc64c0a8e50a0cff91
Alberto Araujo <albertoig8@hotmail.com>
2021-11-01 16:51:37 -0300

    Criando pagina detail

--- PokemonWebApp/Pages/Pokemons/Create.cshtml.cs
+++ PokemonWebApp/Pages/Pokemons/Create.cshtml.cs
@@ -15,7 +15,7 @@ namespace PokemonWebApp.Pages.Pokemons
     {
         // vai comparar os dados do post do fromulario com os atributos do objeto pokemon e vai armazenar na variavel abaixo automaticamente.
         [BindProperty]
-        // instanciando um objeto pokemon para armazenar os dados vindo formulario.
+        // Definindo um atributo pokemon para armazenar os dados vindo formulario.
         public Pokemon Pokemon { get; set; }
 
         // URL base da API.
@@ -24,6 +24,7 @@ namespace PokemonWebApp.Pages.Pokemons
         // OnPostAsync() captura a ação ou as informações disparadas pelo usuario atraves do formulario.
         public async Task<IActionResult> OnPostAsync()
         {
+            // HttpCliente é um serviço que auxilia em chamadas.
             using(var client = new HttpClient())
             {
                 // Passando a base URL para o programa atraves do uri.
@@ -35,7 +36,7 @@ namespace PokemonWebApp.Pages.Pokemons
                     new MediaTypeWithQualityHeaderValue("application/json"));
                 // Defininco uma variavel para receber a resposta e os dados do Post.
                 HttpResponseMessage response = await client
-                    // recebendo o link da API e referenciando o objeto que vai receber os dados.
+                    // recebendo o Rota da API e referenciando o objeto que vai receber os dados.
                     .PostAsJsonAsync("api/Pokemons", Pokemon);
 
                 // Verificando se a resposta veio com sucesso ou não.
---
+++ PokemonWebApp/Pages/Pokemons/Detail.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PokemonWebApp.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace PokemonWebApp.Pages.Pokemons
+{
+    public class DetailModel : PageModel
+    {
+        // Definindo um atributo pokemon para armazenar os dados vindo formulario.
+        public Pokemon Pokemon { get; set; }
+        // URL base da API.
+        string baseUrl = "https://localhost:44375/";
+
+        // Esse metodo é disparado toda vez que o cliente execulta uma ação.
+        // A interrogação indica que esse parameto pode vir vazio.
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("aplication/json"));
+                // recebendo o Rota da API e id do objeto.
+                HttpResponseMessage response = await client.GetAsync("api/Pokemons/" + id);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Pokemon = JsonConvert.DeserializeObject<Pokemon>(result);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
